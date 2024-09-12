ef0923b66cf290a1f8200ce91d82130e26511e7f
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-09-12 20:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	return "abs, acos, acosh, asin, asinh, atan, atan2, atanh, cbrt, ceil, clz32, cos, cosh, exp, expm1, floor, fround, hypot, imul, log, log10, log1p, log2, max, min, pow, random, round, sign, sin, sinh, sqrt, tan, tanh, trunc";
 }
 
 alert(allMethods());
